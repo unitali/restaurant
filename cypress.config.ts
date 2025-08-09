ccf0362461d460359258d6f5e913e55f9893c4d0
origin <rls.rafaelsouza@gmail.com>
2025-08-09 11:41:11 +0200

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
-    supportFile: "support/e2e.ts",
+    supportFile: "cypress/support/e2e.ts",
     specPattern: "cypress/e2e/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
-
+      // implement node event listeners here
     },
   },
 });
